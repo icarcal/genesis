4ba8fea8ff9b2e11fa165d5166944b7f8a1b8827
Icaro Caldeira <icaro.caldeira@hotmail.com>
2018-12-13 03:07:12 -0200

    :sparkles: make create containers possible

--- dist/Genesis.js
+++ dist/Genesis.js
@@ -8,24 +8,143 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const fs = require("fs");
+const os = require("os");
 const Inquirer = require("inquirer");
 const YAML = require("yamljs");
-const node_docker_api_1 = require("node-docker-api");
+const dotenv = require("dotenv");
+const Ora = require("ora");
+const Dockerode = require("dockerode");
+const chalk_1 = require("chalk");
 const options_1 = require("./helpers/options");
 class Genesis {
     static loadDockerCompose() {
+        dotenv.config({
+            path: '../.env',
+        });
         return YAML.load(process.env.DOCKER_COMPOSE_PATH);
+    }
+    static getContainerFileConfigs() {
+        const filePath = '../data/';
+        const fileName = 'selected-containers.json';
+        const fullPath = `${filePath}/${fileName}`;
+        return {
+            filePath,
+            fileName,
+            fullPath,
+        };
+    }
+    static writeOnContainersFile(data = null) {
+        const containerConfig = this.getContainerFileConfigs();
+        const fileData = {
+            containers: data || []
+        };
+        fs.writeFileSync(containerConfig.fullPath, JSON.stringify(fileData));
+    }
+    static definePortMappings(service) {
+        const HOST_PORT = 0;
+        const CONTAINER_PORT = 1;
+        const mapPorts = (port) => {
+            const splitedPorts = port.split(':');
+            const key = `${splitedPorts[CONTAINER_PORT]}/tcp`;
+            const hostPort = {
+                HostPort: splitedPorts[HOST_PORT]
+            };
+            return {
+                [key]: [
+                    hostPort,
+                ]
+            };
+        };
+        const reduceToPortMapType = (prev, next) => {
+            const key = Object.keys(next)[0];
+            prev[key] = next[key];
+            return prev;
+        };
+        const portMaps = service.ports.map(mapPorts)
+            .reduce(reduceToPortMapType, {});
+        return portMaps;
+    }
+    static getContainersFromFile() {
+        const containerConfig = this.getContainerFileConfigs();
+        const containersFile = fs.readdirSync(containerConfig.filePath)
+            .find(file => file === containerConfig.fileName);
+        if (!containersFile) {
+            this.writeOnContainersFile();
+        }
+        const fileBuffer = fs.readFileSync(containerConfig.fullPath);
+        const selectedContainers = JSON.parse(fileBuffer.toString());
+        return selectedContainers;
+    }
+    static createDockerContainer(docker, containerName, service) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const spinner = Ora({
+                text: `Creating ${containerName}`,
+                color: 'green',
+            }).start();
+            const ports = this.definePortMappings(service);
+            const containerConfig = {
+                name: service.container_name,
+                Image: service.image,
+                HostConfig: {
+                    PortBindings: ports,
+                }
+            };
+            if (service.volumes) {
+                containerConfig.HostConfig.Binds = service.volumes;
+            }
+            try {
+                const container = yield docker.createContainer(containerConfig);
+                spinner.stopAndPersist({
+                    symbol: chalk_1.default.blueBright('ℹ'),
+                    text: `Creating ${containerName}`,
+                });
+                spinner.stopAndPersist({
+                    symbol: chalk_1.default.green('✔'),
+                    text: `${containerName} created successfully`,
+                });
+                spinner.start(`Starting ${containerName}`);
+                yield container.start();
+                spinner.stopAndPersist({
+                    symbol: chalk_1.default.blueBright('ℹ'),
+                    text: `Starting ${containerName}`,
+                });
+                spinner.stopAndPersist({
+                    symbol: chalk_1.default.green('✔'),
+                    text: `${containerName} started successfully`,
+                });
+                spinner.stop();
+            }
+            catch (error) {
+                spinner.stopAndPersist({
+                    symbol: chalk_1.default.red('✖'),
+                    text: `${containerName} - ${error.message}`,
+                });
+                spinner.stop();
+            }
+        });
     }
     static requestContainers() {
         return __awaiter(this, void 0, void 0, function* () {
-            const docker = new node_docker_api_1.Docker({ socketPath: '/var/run/docker.sock' });
-            // TODO: READ FROM ALREADY SELECTED
+            dotenv.config();
+            const socketPath = (os.platform() === 'win32')
+                ? 'npipe:////./pipe/docker_engine'
+                : '/var/run/docker.sock';
+            const docker = new Dockerode({ socketPath });
+            const selectedContainers = this.getContainersFromFile();
+            const choicesWithSelected = options_1.default.map((choice) => {
+                const selectedChoice = selectedContainers.containers.find(container => container === choice.value);
+                if (selectedChoice) {
+                    choice.checked = true;
+                }
+                return choice;
+            });
             const answers = yield Inquirer.prompt([
                 {
                     type: 'checkbox',
                     message: 'Select the containers',
                     name: 'containers',
-                    choices: options_1.default,
+                    choices: choicesWithSelected,
                     validate: (answer) => {
                         if (answer.length < 1) {
                             return 'You must choose at least one topping.';
@@ -35,17 +154,61 @@ class Genesis {
                 },
             ]);
             const containersConfig = this.loadDockerCompose();
-            const { containers } = answers;
+            const { containers = [] } = answers;
             const { services } = containersConfig;
-            // TODO: UPDATE ALREADY SELECTED
-            containers.forEach((container) => {
-                const service = Object.keys(services).find(service => service === container);
-                if (service) {
-                    console.log(`${container} not found`);
+            this.writeOnContainersFile(containers);
+            for (const containerName of containers) {
+                const service = services[containerName] || null;
+                const spinner = Ora({
+                    text: `${containerName} is being created`,
+                    color: 'green',
+                }).start();
+                if (!service) {
+                    spinner.stopAndPersist({
+                        symbol: chalk_1.default.red('✖'),
+                        text: `${containerName} not found`,
+                    });
+                    spinner.stop();
                     return;
                 }
-                console.log(`${container} is being created`);
-            });
+                try {
+                    spinner.start(`Cheking if image ${service.image} exists`);
+                    yield new Promise((fulfilled, rejected) => {
+                        docker.pull(service.image, (err, stream) => {
+                            if (err) {
+                                rejected(err);
+                            }
+                            spinner.start(`Downloading image ${service.image}`);
+                            const onFinished = (err) => __awaiter(this, void 0, void 0, function* () {
+                                if (err) {
+                                    spinner.stop();
+                                    rejected(err);
+                                }
+                                spinner.stopAndPersist({
+                                    symbol: chalk_1.default.green('✔'),
+                                    text: `Image ${service.image} downloaded`,
+                                });
+                                spinner.stop();
+                                try {
+                                    yield this.createDockerContainer(docker, containerName, service);
+                                    fulfilled();
+                                }
+                                catch (err) {
+                                    rejected(err);
+                                }
+                            });
+                            docker.modem.followProgress(stream, onFinished, () => { });
+                        });
+                    });
+                }
+                catch (error) {
+                    spinner.stopAndPersist({
+                        symbol: chalk_1.default.red('✖'),
+                        text: `${containerName} - ${error.message}`,
+                    });
+                    spinner.stop();
+                }
+            }
         });
     }
 }
--- src/Genesis.ts
+++ src/Genesis.ts
@@ -1,28 +1,191 @@
+import * as fs from 'fs';
+import * as os from 'os';
 import * as Inquirer from 'inquirer';
 import * as YAML from 'yamljs';
-import { Docker } from 'node-docker-api';
+import * as dotenv from 'dotenv';
+import * as Ora from 'ora';
+import * as Dockerode from 'dockerode';
 import chalk from 'chalk';
 import choices from './helpers/options';
 
 interface ContainerAnswers {
-  containers?: Array<String>
+  containers?: string[]
+}
+
+interface ContainerFileConfig {
+  filePath: string,
+  fileName: string,
+  fullPath: string
+}
+
+interface SelectedContainers {
+  containers: String[]
+}
+
+interface Service {
+  container_name: string,
+  image: string,
+  ports: string[],
+  volumes?: string[],
 }
 
 class Genesis {
   static loadDockerCompose(): Object {
+    dotenv.config({
+      path: '../.env',
+    });
     return YAML.load(process.env.DOCKER_COMPOSE_PATH);
   }
 
+  static getContainerFileConfigs(): ContainerFileConfig {
+    const filePath: string = '../data/';
+    const fileName: string = 'selected-containers.json';
+    const fullPath: string = `${filePath}/${fileName}`;
+
+    return {
+      filePath,
+      fileName,
+      fullPath,
+    }
+  }
+
+  static writeOnContainersFile(data: string[] = null): void {
+    const containerConfig: ContainerFileConfig = this.getContainerFileConfigs();
+
+    const fileData = {
+      containers: data || []
+    };
+
+    fs.writeFileSync(
+      containerConfig.fullPath,
+      JSON.stringify(fileData)
+    );
+  }
+
+  static definePortMappings(service: Service): Dockerode.PortMap {
+    const HOST_PORT: number = 0;
+    const CONTAINER_PORT: number = 1;
+
+    const mapPorts = (port: string) => {
+      const splitedPorts: string[] = port.split(':');
+      const key: string = `${splitedPorts[CONTAINER_PORT]}/tcp`;
+      const hostPort: Dockerode.PortBinding = {
+        HostPort: splitedPorts[HOST_PORT]
+      };
+
+      return {
+        [key]: [
+          hostPort,
+        ]
+      }
+    };
+
+    const reduceToPortMapType = (prev: Object, next: Object) => {
+      const key: string = Object.keys(next)[0];
+      prev[key] = next[key];
+      return prev;
+    }
+
+    const portMaps:Dockerode.PortMap = service.ports.map(mapPorts)
+      .reduce(reduceToPortMapType, {});
+
+    return portMaps;
+  }
+
+  static getContainersFromFile(): SelectedContainers {
+    const containerConfig: ContainerFileConfig = this.getContainerFileConfigs();
+    const containersFile: string = fs.readdirSync(containerConfig.filePath)
+      .find(file => file === containerConfig.fileName);
+
+    if (!containersFile) {
+      this.writeOnContainersFile();
+    }
+
+    const fileBuffer: Buffer = fs.readFileSync(containerConfig.fullPath);
+    const selectedContainers: SelectedContainers = JSON.parse(fileBuffer.toString());
+
+    return selectedContainers;
+  }
+
+  static async createDockerContainer(docker: Dockerode, containerName: string, service: Service) {
+    const spinner: any = Ora({
+      text: `Creating ${containerName}`,
+      color: 'green',
+    }).start();
+    const ports: Dockerode.PortMap = this.definePortMappings(service);
+    const containerConfig: Dockerode.ContainerCreateOptions = {
+      name: service.container_name,
+      Image: service.image,
+      HostConfig: {
+        PortBindings: ports,
+      }
+    }
+
+    if (service.volumes) {
+      containerConfig.HostConfig.Binds = service.volumes
+    }
+
+    try {
+      const container: Dockerode.Container = await docker.createContainer(containerConfig);
+      spinner.stopAndPersist({
+        symbol: chalk.blueBright('ℹ'),
+        text: `Creating ${containerName}`,
+      });
+
+      spinner.stopAndPersist({
+        symbol: chalk.green('✔'),
+        text: `${containerName} created successfully`,
+      });
+
+      spinner.start(`Starting ${containerName}`);
+      await container.start();
+      spinner.stopAndPersist({
+        symbol: chalk.blueBright('ℹ'),
+        text: `Starting ${containerName}`,
+      });
+
+      spinner.stopAndPersist({
+        symbol: chalk.green('✔'),
+        text: `${containerName} started successfully`,
+      });
+
+      spinner.stop();
+    } catch (error) {
+      spinner.stopAndPersist({
+        symbol: chalk.red('✖'),
+        text: `${containerName} - ${error.message}`,
+      });
+
+      spinner.stop();
+    }
+
+  }
+
   static async requestContainers() {
-    const docker = new Docker({ socketPath: '/var/run/docker.sock' })
-    // TODO: READ FROM ALREADY SELECTED
+    dotenv.config();
+
+    const socketPath = (os.platform() === 'win32')
+      ? 'npipe:////./pipe/docker_engine'
+      : '/var/run/docker.sock';
+    const docker = new Dockerode({ socketPath });
+
+    const selectedContainers: SelectedContainers = this.getContainersFromFile();
+    const choicesWithSelected: Object[] = choices.map((choice: Inquirer.Answers) => {
+      const selectedChoice: String = selectedContainers.containers.find(container => container === choice.value);
+
+      if (selectedChoice) {
+        choice.checked = true;
+      }
+
+      return choice;
+    });
 
     const answers: ContainerAnswers = await Inquirer.prompt([
       {
         type: 'checkbox',
         message: 'Select the containers',
         name: 'containers',
-        choices: choices,
+        choices: choicesWithSelected,
         validate: (answer) => {
           if (answer.length < 1) {
             return 'You must choose at least one topping.';
@@ -34,20 +197,71 @@ class Genesis {
     ]);
 
     const containersConfig: Object = this.loadDockerCompose();
-    const { containers } = answers;
+    const { containers = [] } : { containers?: string[]} = answers;
     const { services }: any = containersConfig;
 
-    // TODO: UPDATE ALREADY SELECTED
+    this.writeOnContainersFile(containers);
 
-    containers.forEach((container: String) => {
-      const service = Object.keys(services).find(service => service === container);
-      if (service) {
-        console.log(`${container} not found` );
+    for (const containerName of containers) {
+      const service: Service = services[containerName] || null;
+
+      const spinner: any = Ora({
+        text: `${containerName} is being created`,
+        color: 'green',
+      }).start();
+
+      if (!service) {
+        spinner.stopAndPersist({
+          symbol: chalk.red('✖'),
+          text: `${containerName} not found`,
+        });
+        spinner.stop();
         return;
       }
 
-      console.log(`${container} is being created` );
-    });
+      try {
+        spinner.start(`Cheking if image ${service.image} exists`);
+
+        await new Promise((fulfilled, rejected) => {
+          docker.pull(service.image, (err, stream) => {
+            if (err) {
+              rejected(err);
+            }
+
+            spinner.start(`Downloading image ${service.image}`);
+
+            const onFinished = async (err) => {
+              if (err) {
+                spinner.stop();
+                rejected(err);
+              }
+
+              spinner.stopAndPersist({
+                symbol: chalk.green('✔'),
+                text: `Image ${service.image} downloaded`,
+              });
+              spinner.stop();
+
+              try {
+                await this.createDockerContainer(docker, containerName, service);
+                fulfilled();
+              } catch(err) {
+                rejected(err);
+              }
+            }
+
+            docker.modem.followProgress(stream, onFinished, () => {});
+          });
+        });
+      } catch (error) {
+        spinner.stopAndPersist({
+          symbol: chalk.red('✖'),
+          text: `${containerName} - ${error.message}`,
+        });
+
+        spinner.stop();
+      }
+    }
   }
 }
 
