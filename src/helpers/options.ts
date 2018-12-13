db82c261be268c923031dc133122b9dcfa04a1f6
Icaro Caldeira <icaro.caldeira@hotmail.com>
2018-12-13 03:06:15 -0200

    :fire: remove not neede containers

--- src/helpers/options.ts
+++ src/helpers/options.ts
@@ -1,16 +1,18 @@
 import * as Inquirer from 'inquirer';
 
 const choices: Array<Inquirer.Answers> = [
-  { value: 'app-service', name: 'App Service' },
-  { value: 'media-tool', name: 'Media tool' },
-  { value: 'mma-front', name: 'MMA Front' },
-  { value: 'mma-service', name: 'MMA Service' },
+  // { value: 'app-service', name: 'App Service' },
+  // { value: 'media-tool', name: 'Media tool' },
+  // { value: 'mma-front', name: 'MMA Front' },
+  // { value: 'mma-service', name: 'MMA Service' },
+  { value: 'mailcatcher', name: 'Mail Catcher' },
   { value: 'mongo', name: 'MongoDB' },
-  { value: 'mysql', name: 'Mysql' },
-  { value: 'ssh', name: 'SSH' },
-  { value: 'rabbit', name: 'RabbitMQ' },
-  { value: 'user-service', name: 'User Service' },
-  { value: 'tool-front-end', name: 'Tool' },
+  // { value: 'mysql', name: 'Mysql' },
+  // { value: 'ssh', name: 'SSH' },
+  { value: 'rabbitmq', name: 'RabbitMQ' },
+  { value: 'redis', name: 'Redis' }
+  // { value: 'user-service', name: 'User Service' },
+  // { value: 'tool-front-end', name: 'Tool' },
 ];
 
 export default choices;
